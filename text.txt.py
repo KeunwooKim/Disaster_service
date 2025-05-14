e30f29d3d30998a584e48e546f2ee7b751774944
KeunwooKim <andy_0613@naver.com>
2025-05-14 17:33:35 +0900

    1

--- text.txt.py
+++ text.txt.py
@@ -1,7 +1,2 @@
-CREATE TABLE IF NOT EXISTS ForecastAnnouncement (
-    announce_no uuid PRIMARY KEY,
-    disaster_region text,
-    alert_type text,
-    announce_time timestamp,
-    comment text
-);
+DELETE FROM rtd_db WHERE rtd_code = 33 AND rtd_time IN ('2025-04-02 00:00:00', '2025-04-05 12:00:00') AND id IN (uuid1, uuid2);
+SELECT rtd_code, rtd_time, id FROM rtd_db WHERE rtd_time > '2025-04-01 00:00:00' ALLOW FILTERING;
